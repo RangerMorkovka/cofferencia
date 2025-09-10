8d83c2e9ec786c91334c8b02b6139e10daf6b970
RangerMorkovka <artka3003@gmail.com>
2025-09-10 21:09:20 +1000

    begin

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+const App = function App() {
+  
+
+  return (
+    <body>
+    <header>
+      <h1>Акции</h1>
+    </header> 
+    </body>
+  )
+}
+
+export default App
